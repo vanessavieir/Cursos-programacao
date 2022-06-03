30b605d830160895be6eae6df57437b78c9c2c77
vanessavieir <vanessa2209vieira@gmail.com>
2022-06-03 20:38:51 -0300

    aulas

---
+++ PYTHON/aula02/ex01.py
@@ -0,0 +1,4 @@
+n=int(input('Digite um número: '))
+n1=int(input('Digite outro número: '))
+s=n+n1
+print('A soma entre {} e {} resulta no número {}'.format(n,n1,s))
---
+++ PYTHON/aula02/ex02.py
@@ -0,0 +1,8 @@
+n=input('Digite algo: ')
+print('O tipo primitivo de A corresponde a ', type(n))
+print('Só tem espaço',n.isspace())
+print('É um número?', n.isnumeric())
+print('É alfanúmerico?',n.isalnum())
+print('Contém letras maiúsculas?',n.isupper())
+print('Contém letras minúsculas?',n.islower())
+print('Está capitalizada?', n.istitle()) #contém letras maiúsculas e minúsculas
