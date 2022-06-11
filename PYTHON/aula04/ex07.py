ca16f752308ac5d88f3cce3ac4df93d495efb0d5
vanessavieir <vanessa2209vieira@gmail.com>
2022-06-11 12:41:49 -0300

    exercícios

---
+++ PYTHON/aula04/ex07.py
@@ -0,0 +1,11 @@
+# nome=str(input('Digite seu mome completo: ')).strip()
+# print('Seu nome têm Ferreira no nome? {}'.format('FERREIRA'in nome.upper()))
+
+# from itertools import count
+
+
+# frase=str(input('Digite um texto pequeno ou frase: ')).upper().strip()
+# print('A letra "E"  aparece quantas vezes na frase que você digitou? {}'.format(frase.count('E')))
+# print('Onde aparece a primeira ocorrência de E? {}'.format(frase.find('E')+1))
+# print('Onde aparece a última ocorrência de E? {}'.format(frase.rfind('E')+1))
+
---
+++ PYTHON/aula05/ex01.py
@@ -0,0 +1,15 @@
+# nome=input('Digite seu nome:')
+# if nome=='Vanessa':
+#     print('Seu nome é muito bonito!')
+# else:
+#     print('Seu nome é tão normal!')
+# print('Seja bem vindo(a) {}!'.format(nome))
+
+# n1=float(input('Digite a primeira nota:'))
+# n2=float(input('Digite a segunda nota:'))
+# media=(n1+n2)/2
+# print('A média da soma das notas {} e {} é {}'.format(n1 , n2, media))
+# if media <6.0:
+#     print('Você está reprovado')
+# else:
+#     print('Você está aprovado')
